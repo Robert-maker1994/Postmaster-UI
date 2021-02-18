302000d04562aa584d5be91898b84690492556cb
Robert <robertpaullawson@gmail.com>
2021-02-18 13:59:56 +0000

    aboutpage

--- src/Routing/Routing.js
+++ src/Routing/Routing.js
@@ -8,7 +8,7 @@ import {
 // Importing automatic Dynamic Imports
  const HomePage = React.lazy(() => import("../Pages/Home"));
  const blogPage = React.lazy(() => import("../Pages/Blog"));
- const aboutPage = React.lazy(() => import("../Pages/About"));
+ const About = React.lazy(() => import("../Pages/About"));
  const Post = React.lazy(() => import("../Components/Posts/Posts"));
  const Featured = React.lazy(() => import("../Components/Categories/Featured"));
  const Cooking = React.lazy(() => import("../Components/Categories/Cooking"));
@@ -29,7 +29,7 @@ function Routing() {
     <Switch>
       <Route exact path={home} component={HomePage} />
       <Route path={blog} component={blogPage} />
-      <Route path={about} component={aboutPage} />
+      <Route path={about} component={About} />
       <Route path="/post/:postId" component={Post} />
       <Route path={featured} component={Featured} />
       <Route path={book} component={Book} />
